66e60b99ec0c44f10d0266e39cd3fffe9db776bd
umapathi s <umapathi.developer78@gmail.com>
2019-08-05 20:42:38 +0530

    First Commit

---
+++ securepump/src/test/java/com/securepump/securepump/SecurepumpApplicationTests.java
@@ -0,0 +1,16 @@
+package com.securepump.securepump;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurepumpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
