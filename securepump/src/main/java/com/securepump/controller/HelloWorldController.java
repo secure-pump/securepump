fcb88070be18c324a58bf670e640b302256730f7
naresh <naresh@DESKTOP-HH7SQAF>
2019-08-15 00:11:19 +0530

    code added

--- securepump/src/main/java/com/securepump/controller/HelloWorldController.java
+++ securepump/src/main/java/com/securepump/controller/HelloWorldController.java
@@ -1,4 +1,4 @@
-package com.securepump.securepump.controller;
+package com.securepump.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
--- securepump/src/main/java/com/securepump/controller/TankTranferController.java
+++ securepump/src/main/java/com/securepump/controller/TankTranferController.java
@@ -1,4 +1,4 @@
-package com.securepump.securepump.controller;
+package com.securepump.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
