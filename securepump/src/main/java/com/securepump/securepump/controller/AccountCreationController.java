339b22d5445e0031deb78b1c6f788ca9d28b045b
naresh <naresh@DESKTOP-HH7SQAF>
2019-08-18 22:17:07 +0530

    Complete view page was done for account creation

--- securepump/src/main/java/com/securepump/securepump/controller/AccountCreationController.java
+++ securepump/src/main/java/com/securepump/securepump/controller/AccountCreationController.java
@@ -3,17 +3,21 @@ package com.securepump.securepump.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
 import com.securepump.securepump.exception.RecordNotFoundException;
 import com.securepump.securepump.model.AccountCreationEntity;
 import com.securepump.securepump.service.AccountCreationService;
 
+@Configurable
 @Controller
 public class AccountCreationController {
 	
@@ -21,20 +25,29 @@ public class AccountCreationController {
 	AccountCreationService accountService;
 
 	@RequestMapping("/account-creation")
-	public String AccountCreate(@ModelAttribute("account") AccountCreationEntity account,Model model) {
+	public String AccountCreate(@ModelAttribute("account") AccountCreationEntity account,Model model,@RequestParam(name = "status") String status) {
 		List<AccountCreationEntity> listAccounts = accountService.getAllAccounts();		
 	    model.addAttribute("listAccounts", listAccounts);
-	   // model.addAttribute("account", account);
+	  System.out.println("status--"+status);
+	   model.addAttribute("status", status);
 		return "account-creation";
 	}
 	@RequestMapping(value = "/saveAccount", method = RequestMethod.POST)
 	public String saveProduct(@ModelAttribute("account") AccountCreationEntity account,Model model) {
-		System.out.println("account--"+account);
-		accountService.createOrUpdateAccounts(account);
-		//List<AccountCreationEntity> listAccounts = accountService.getAllAccounts();
-		//System.out.println("listAccounts--"+listAccounts.size());
-	   // model.addAttribute("listAccounts", listAccounts);
-	    return "redirect:/account-creation?status=de";
+		//System.out.println("account--"+account);
+		String status="error";
+		try {
+			if(account.getId()==null) {
+				status="save";
+			}else {
+				status="update";
+			}
+			accountService.createOrUpdateAccounts(account);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	    return "redirect:/account-creation?status="+status;
 	}
 	@RequestMapping("/accountEdit/{id}")
 	public ModelAndView showEditProductPage(@PathVariable(name = "id") Long id) throws RecordNotFoundException {
@@ -47,8 +60,6 @@ public class AccountCreationController {
 	@RequestMapping("/accountDelete/{id}")
 	public String deleteProduct(@PathVariable(name = "id") Long id,Model model) throws RecordNotFoundException {
 		accountService.deleteAccountsById(id);
-		//List<AccountCreationEntity> listAccounts = accountService.getAllAccounts();
-	    //model.addAttribute("listAccounts", listAccounts);
-		 return "redirect:/account-creation";
+		 return "redirect:/account-creation?status=delete";
 	}
 }
