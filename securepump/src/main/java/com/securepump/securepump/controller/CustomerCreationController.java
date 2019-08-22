8cd1561dadf2a3931d4cfdd3955b92619f8ef54a
naresh <naresh@DESKTOP-HH7SQAF>
2019-08-22 22:04:43 +0530

    ui changes added

--- securepump/src/main/java/com/securepump/securepump/controller/CustomerCreationController.java
+++ securepump/src/main/java/com/securepump/securepump/controller/CustomerCreationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.securepump.securepump.exception.RecordNotFoundException;
@@ -20,19 +21,29 @@ public class CustomerCreationController {
 	@Autowired
 	CustomerCreationService customerService;
 	@RequestMapping("/customer-cretion") 
-	public String customercreate(@ModelAttribute("customer") CustomerCreationEntity customer,Model model ) {
+	public String customercreate(@ModelAttribute("customer") CustomerCreationEntity customer,Model model,@RequestParam(name = "status") String status ) {
 		List<CustomerCreationEntity> listCustomer=customerService.getAllCutomer();
 		model.addAttribute("listCustomer",listCustomer);
+		model.addAttribute("status", status);
 		return "customer-creation";
 	}
 	@RequestMapping(value = "/saveCustomer", method = RequestMethod.POST)
 	public String saveCustomerDetails(@ModelAttribute("customer") CustomerCreationEntity customer,Model model) {
 		System.out.println("Save/Update Customer Details"+customer);
-		customerService.createOrUpdateCustomer(customer);
-		List<CustomerCreationEntity> listCustomer=customerService.getAllCutomer();
-		System.out.println("Customer List Size=="+listCustomer.size());
-		model.addAttribute("listCustomers",listCustomer);
-		return "customer-creation";
+		String status="error";
+		try {
+			if(customer.getId()==null) {
+				status="save";
+			}else {
+				status="update";
+			}
+			customerService.createOrUpdateCustomer(customer);
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	    return "redirect:/boy-creation?status="+status;
+		
 	}
 	
 	@RequestMapping("/customerEdit/{id}")
@@ -45,6 +56,6 @@ public class CustomerCreationController {
 	@RequestMapping("/customerDelete/{id}")
 	public String deleteCustomer(@PathVariable(name = "id") Long id,Model model) throws RecordNotFoundException {
 		customerService.deleteCustomerById(id);
-		return "/cutomer-creation";
+		return "redirect:/cutomer-creation?status=delete";
 	}
 }
