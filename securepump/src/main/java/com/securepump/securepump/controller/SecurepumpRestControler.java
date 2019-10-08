722df01ab58593701338094df6ee3bf1f521e678
N1RESH14 <53684177+N1RESH14@users.noreply.github.com>
2019-10-08 22:33:17 +0530

    purchace rest controler
    purchace rest controler

---
+++ securepump/src/main/java/com/securepump/securepump/controller/SecurepumpRestControler.java
@@ -0,0 +1,60 @@
+package com.securepump.securepump.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.securepump.securepump.bean.PurchaceEntryRestVO;
+import com.securepump.securepump.bean.PurchaseChaildRestVo;
+import com.securepump.securepump.exception.RecordNotFoundException;
+import com.securepump.securepump.model.PurchaseChaildEntity;
+import com.securepump.securepump.model.PurchaseEntryEntity;
+import com.securepump.securepump.service.PurchaceEntryService;
+
+@RestController
+public class SecurepumpRestControler {
+	@Autowired
+	@Qualifier("purchaceservice")
+	PurchaceEntryService purchaceEntryService;
+	
+	@RequestMapping("/purchaceEntryEdit")
+    public PurchaceEntryRestVO processAJAXRequest(@RequestParam("purchaceid") Long purchaceid) throws RecordNotFoundException {
+        
+        System.out.println("working--"+purchaceid);
+        PurchaseEntryEntity purchaseEntryData = purchaceEntryService.getItemById(purchaceid);
+        PurchaceEntryRestVO restVo=new PurchaceEntryRestVO();
+        restVo.setId(purchaseEntryData.getId());
+        restVo.setInvoiceDate(purchaseEntryData.getInvoiceDate());
+        restVo.setBillMode(purchaseEntryData.getBillMode());
+        restVo.setSupplierID(purchaseEntryData.getSupplierCreation().getId());
+        restVo.setSupplierName(purchaseEntryData.getSupplierCreation().getSupplierName());
+        restVo.setSupplierInvoiceNum(purchaseEntryData.getSupplierInvoiceNum());
+        restVo.setInvoiceDate(purchaseEntryData.getInvoiceDate());
+        List<PurchaseChaildEntity> purchaseChaildEntity=purchaseEntryData.getPurchaseChaildEntity();
+        List<PurchaseChaildRestVo> chaildListVo=new ArrayList<PurchaseChaildRestVo>();
+        for(PurchaseChaildEntity purChaildEnt:purchaseChaildEntity){
+        	PurchaseChaildRestVo purChaildVo=new PurchaseChaildRestVo();
+        	purChaildVo.setPurchaceChaildId(purChaildEnt.getId());
+        	purChaildVo.setItemId(purChaildEnt.getItemCreation().getId());
+        	purChaildVo.setItemName(purChaildEnt.getItemCreation().getItemName());
+        	purChaildVo.setItemNature(purChaildEnt.getItemCreation().getItemNature());
+        	purChaildVo.setQuantity(purChaildEnt.getQuantity());
+        	purChaildVo.setRate(purChaildEnt.getRate());
+        	purChaildVo.setAmount(purChaildEnt.getAmount());
+        	purChaildVo.setCgst(purChaildEnt.getCgst());
+        	purChaildVo.setIgst(purChaildEnt.getIgst());
+        	purChaildVo.setSgst(purChaildEnt.getSgst());
+        	purChaildVo.setNetAmt(purChaildEnt.getNetAmt());
+        	chaildListVo.add(purChaildVo);
+        }
+        restVo.setPurchaseChaildRestVo(chaildListVo);
+        System.out.println("----chaildListVo--"+chaildListVo.size());
+        return restVo;
+    }
+
+}
