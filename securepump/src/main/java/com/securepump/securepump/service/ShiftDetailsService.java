d7245c518559abee697e310efe327f0542c8f84e
N1RESH14 <53684177+N1RESH14@users.noreply.github.com>
2019-10-21 23:46:26 +0530

    shiftDetails added

---
+++ securepump/src/main/java/com/securepump/securepump/service/ShiftDetailsService.java
@@ -0,0 +1,18 @@
+package com.securepump.securepump.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.securepump.securepump.exception.RecordNotFoundException;
+import com.securepump.securepump.model.ShiftDetailsEntity;
+@Service("shiftDetails")
+public interface ShiftDetailsService {
+	public List<ShiftDetailsEntity> getAllShiftDetails();
+	public ShiftDetailsEntity getShiftDetailsById(Long id) throws RecordNotFoundException;
+	public ShiftDetailsEntity createOrUpdateShiftDetails(ShiftDetailsEntity entity);
+	public void deleteShiftDetailsById(Long id) throws RecordNotFoundException;
+	public List<String> findByNozzleName(Long id);
+	public int findByNozzleReading(String nozzleName);
+
+}
